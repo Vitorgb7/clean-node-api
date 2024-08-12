de233db78099e55b40e8efe8df18425b9f32d461
vitor <vitorgabriel181004@gmail.com>
2024-08-12 16:00:37 -0300

    refactor: move EmailValidator creation a factory helper method

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -8,13 +8,27 @@ interface SutType {
     emailValidatorStub: EmailValidator
 }
 
-const makeSut = (): SutType => {
+const makeEmailValidator = (): EmailValidator => {
     class EmailValidatorStub implements EmailValidator {
         isValid(email: string): boolean {
-            return false
+            return true
         }
     }
-    const emailValidatorStub = new EmailValidatorStub()
+    return new EmailValidatorStub()
+}
+const makeEmailValidatorWithError = (): EmailValidator => {
+    class EmailValidatorStub implements EmailValidator {
+        isValid(email: string): boolean {
+            throw new Error()
+        }
+    }
+    return new EmailValidatorStub()
+}
+
+
+
+const makeSut = (): SutType => {
+    const emailValidatorStub = makeEmailValidator()
     const sut =  new SignUpController(emailValidatorStub)
     return {
         sut,
@@ -105,12 +119,7 @@ describe('SignUp Controller', () => {
         expect(isValidSpy).toHaveBeenCalledWith('any_email@mail.com')
     })
     test('shold return 500 if EmailValidator throws', () => {
-        class EmailValidatorStub implements EmailValidator {
-            isValid(email: string): boolean {
-                throw new Error()
-            }
-        }
-        const emailValidatorStub = new EmailValidatorStub()
+        const emailValidatorStub = makeEmailValidatorWithError()
         const sut =  new SignUpController(emailValidatorStub)
         const httpRequest = {
             body: {
