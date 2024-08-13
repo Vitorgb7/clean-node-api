cc86d26522760002863cb2397e234c900492da3a
vitor <vitorgabriel181004@gmail.com>
2024-08-13 12:01:14 -0300

    test: ensure SignUpController returns 200 if valid data is provided

--- src/presentation/controllers/signup/signup.spec.ts
+++ src/presentation/controllers/signup/signup.spec.ts
@@ -111,6 +111,7 @@ describe('SignUp Controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new InvalidParamsErrors('passwordConfirmation'))
     })
+
     test('shold return 400 if an invalid email is provided', () => {
         const { sut, emailValidatorStub } = makeSut()
         jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
@@ -192,6 +193,25 @@ describe('SignUp Controller', () => {
             name: 'any_name',
             email: 'any_email@mail.com',
             password: 'any_password'
+        })
+    })
+    test('shold return 200 if valid data is provided', () => {
+        const { sut } = makeSut()
+        const httpRequest = {
+            body: {
+                name: 'valid',
+                email: 'valid_email@mail.com',
+                password: 'valid_password',
+                passwordConfirmation: 'valid_password'
+            }
+        }
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(200)
+        expect(httpResponse.body).toEqual({
+            id: 'valid_id',
+            name: 'valid_name',
+            email: 'valid_email@mail.com',
+            password: 'valid_password'
         })
     })
 })  
